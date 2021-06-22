63877c61b7dbe33004261397e5030b35c9cee283
edelmutic <edelmutic@gmail.com>
2021-06-22 15:44:34 +0300

    add dotenv configuratin

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+require('dotenv').config();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const start = () => {
   try {
