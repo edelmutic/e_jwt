c8e6b5c39afcca29e25706b9633a13d269fbaf98
edelmutic <edelmutic@gmail.com>
2021-06-23 11:13:27 +0300

    add client-server middlewares

--- server/index.js
+++ server/index.js
@@ -7,6 +7,10 @@ const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+
 const start = async () => {
   try {
     await mongoose.connect(process.env.DB_URL, {
