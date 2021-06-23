4cc37c35ee43f13b26ff89ebe4ea3e63eb8d079d
edelmutic <edelmutic@gmail.com>
2021-06-23 11:09:09 +0300

    add mongoose connection

--- server/index.js
+++ server/index.js
@@ -2,12 +2,17 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 require('dotenv').config();
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const start = () => {
+const start = async () => {
   try {
+    await mongoose.connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     app.listen(PORT, () => {
       console.log(`App listening on ${PORT}`);
     });
