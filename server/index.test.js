5150e484f81b4b620959063739049172b0dc768e
edelmutic <edelmutic@gmail.com>
2021-06-25 15:20:53 +0300

    little updates

--- server/index.test.js
+++ server/index.test.js
@@ -1,6 +1,6 @@
 const request = require('supertest');
 
-const app = require('./app').app;
+const app = require('./index').app;
 
 it('should return Hello Test', function (done) {
   request(app).get('/test').expect('Hello from test').end(done);
