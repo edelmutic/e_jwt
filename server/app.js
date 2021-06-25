b31a37165e71531f95c584746c97ddb38ce64de8
edelmutic <edelmutic@gmail.com>
2021-06-25 15:15:15 +0300

    update main server file

---
+++ server/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+
+app.get('/test', (req, res) => {
+  res.send('Hello from test');
+});
+
+const start = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    app.listen(PORT, () => {
+      console.log(`App listening on ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
+
+module.exports.app = app;
