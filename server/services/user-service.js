d225123e29eb48aacc6c544e98aa37450a4abf7e
edelmutic <edelmutic@gmail.com>
2021-06-30 13:12:30 +0300

    create registration service with db check-in

--- server/services/user-service.js
+++ server/services/user-service.js
@@ -1,7 +1,13 @@
 const UserModel = require('../models/user-models');
 
 class UserService {
-  async registration(email, password) {}
+  async registration(email, password) {
+    const candidate = await UserModel.findOne({ email });
+    if (candidate) {
+      throw new Error(`User with such ${email} already exist`);
+    }
+    const user = await UserModel.create({ email, password });
+  }
 }
 
 module.exports = new UserService();
