9dd681c017f2c845124560def4c8162ee67f3733
edelmutic <edelmutic@gmail.com>
2021-07-14 22:57:45 +0300

    update cors credentials

--- server/index.js
+++ server/index.js
@@ -9,7 +9,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL,
+  })
+);
 app.use(cookieParser());
 app.use('/api', router);
 
